7c4cfd992f6cc1d4c11c2c193541d90db867d77d
suhasini <suhasiniabhishal49@gmail.com>
2025-11-27 10:26:14 +0530

    pythin added

---
+++ hello.py
@@ -0,0 +1,50 @@
+# print ("hello world")
+#variables 1-primitive
+# a = 12
+# b = 78
+# c = 4
+# a = b
+# b = c
+# c = a 
+# print(a)
+# print(b)
+# print(c)
+# a = input("Enter the value:")
+# print(a)
+# b = "5"
+# print (b)
+# print(type(a))
+# print(type(b))
+# # primitive datatypes - int,float,str,boolean
+# a = int(input("enter the value: "))
+# print(a)
+# b = float(input("enter the value: "))
+# print(b)
+# print(type(a))
+# print(type(b))
+# name=("suhasini","abhishal",40,45)
+# print(name)
+# ages=[56,67,45,65]
+# print(ages)
+# print(type(name))
+# print(type(name))
+
+# #even/odd
+# a = int(input("enter the value"))
+# if a%2 == 0:
+#     print("The number is Even ")
+# else:
+#     print("Yhe number is odd")
+
+#nested if else
+age = 20
+print("age is:", age)
+
+if age < 15:
+    print("not eligible")
+elif age <= 18:
+    print("eligible to ride")
+elif age > 60:
+    print("senior citizen cannot ride")
+else:
+    print("eligible to ride")
